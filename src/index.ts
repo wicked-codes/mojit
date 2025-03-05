8583a3f1ced0289b48750b09e29141f66199af8d
wicked-codes <ashishkumarmohan5@gmail.com>
2025-03-05 23:07:24 +0530

    Formatting issues

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import * as process from 'process';
 import yargs from 'yargs';
 import { commandFunctionMap } from './commandChain';
 import { supportedOptions } from './config/supportedOptions.config';
-async function parseArgs(){
+
+async function parseArgs() {
     const argv = await yargs(process.argv.slice(2)).parse();
     const args = argv._;
-    if(args.length === 0){
+    if (args.length === 0) {
         console.log('Please provide a command');
         process.exit(1);
     }
@@ -17,21 +18,21 @@ async function parseArgs(){
         return (key !== '_' && key !== '$0') && value;
     }));
     console.log(commandOptions)
-    return {command, commandOptions};
+    return { command, commandOptions };
 }
 
-async function getRequiredPaths(){
+async function getRequiredPaths() {
     const directoryFilePath = process.cwd();
     const repoPath = path.join(directoryFilePath, '.mojit');
     const currentFolderName = path.basename(directoryFilePath);
-    return {directoryFilePath, repoPath, currentFolderName};
+    return { directoryFilePath, repoPath, currentFolderName };
 }
 
-async function execute(){
-    const {command, commandOptions} = await parseArgs();
+async function execute() {
+    const { command, commandOptions } = await parseArgs();
     const requiredPaths = await getRequiredPaths();
-    if(commandFunctionMap[command]){
-        commandFunctionMap[command](requiredPaths, commandOptions as Record<string, string|boolean|number>, supportedOptions[command]);
+    if (commandFunctionMap[command]) {
+        commandFunctionMap[command](requiredPaths, commandOptions as Record<string, string | boolean | number>, supportedOptions[command]);
     } else {
         console.log(`Command not found: ${command}`);
         process.exit(1);
